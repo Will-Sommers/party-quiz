b6af1b00e08a6db691688c08747a3a140288b6d0
Will Sommers <will.sommers@gmail.com>
2014-06-23 23:46:53 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PartyQuiz::Application.config.secret_key_base = '56189d7fe4b3667ec1640028009009652ede1b7adba131479f5e07991118e709f209aed059efd808b51228b7bbedc87d302b97da6f4e34db2bdf88089e1e04c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PartyQuiz::Application.config.session_store :cookie_store, key: '_party-quiz_session'
